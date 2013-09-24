77334e16a3b9c36177a0764e45a2fafd14de05b5
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-09-24 20:59:29 +0100

    Added filter for owner same as current content item.

---
+++ Filters/OwnerIsCurrentAuthor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Orchard.ContentManagement;
+using Orchard.Core.Common.Models;
+using Orchard.Localization;
+using Orchard.Projections.Descriptors.Filter;
+using Orchard.Projections.Services;
+
+
+namespace NogginBox.OwnerQueries.Filters
+{
+	public class OwnerIsCurrentAuthor : IFilterProvider
+	{
+		private readonly ICurrentContentAccessor _currentContentAccessor;
+
+		public OwnerIsCurrentAuthor(ICurrentContentAccessor workContextAccessor)
+		{
+			_currentContentAccessor = workContextAccessor;
+		}
+
+		public Localizer T { get; set; }
+
+		public void Describe(DescribeFilterContext describe) {
+			describe.For("ContentOwner", T("Content Owner"), T("The owner of the content item"))
+				.Element("AuthorOwnerMatch", T("Author of current content"), T("Is owned by the author of the page's main document. Works best with widgets."),
+					ApplyFilter,
+					context => T("Content is owned by main document author")
+				);
+		}
+
+		public void ApplyFilter(FilterContext context)
+		{
+			var currentContent = _currentContentAccessor.CurrentContentItem;
+			if (currentContent == null) return;
+
+			var commonPart = currentContent.As<CommonPart>();
+			if (commonPart == null) return;
+
+			Action<IAliasFactory> selector = alias => alias.ContentPartRecord<CommonPartRecord>();
+			Action<IHqlExpressionFactory> filter = x => x.Eq("OwnerId", commonPart.Owner.Id);
+			context.Query.Where(selector, filter);
+		}
+	}
+}
